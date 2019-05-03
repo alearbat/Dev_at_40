bc9cc410b0e9ba16525343445d0ea6eb7ccffb33
alearbat <alearbat@gmail.com>
2019-05-03 19:21:23 -0300

    adding JS page and reversing order of posts

--- assets/js/jstest.js
+++ assets/js/jstest.js
@@ -1,3 +1,25 @@
 function myFunction() {
   document.getElementById("demo").innerHTML = "Hello JavaScript!";
 }
+
+  var mult = "";
+
+  var input = 1;
+  while (input <= 100) {
+    if (input % 3 == 0 && input % 5 == 0) {
+      mult = mult + " FizzBuzz";
+    } else if (input % 3 == 0) {
+      mult = mult + " Fizz";
+    } else if (input % 5 == 0) {
+      mult = mult + " Buzz";
+    } else {
+      mult = mult + " " + (input);
+    }
+    input++;
+  }
+  function oneToOneHundred() {
+    document.getElementById("numbers").innerHTML = mult;
+  }
+
+ 
+  
